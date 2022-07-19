14dd797f4eff01e27bbb726b81613765ce720bc3
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-07-18 23:41:48 -0300

    Created the first screen of the page

---
+++ src/components/EnterScreen.js
@@ -0,0 +1,9 @@
+export default function EnterScreen() {
+    return (
+        <section className="screenOne">
+            <img src="/assets/img/big-logo.png" alt=""/>
+            <h1>ZapRecall</h1>
+            <button><p>Iniciar Recall!</p></button>
+        </section>
+    )
+}
