4a6bcd9002357f24b06402e30beaaf4aa4b68319
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-07-21 01:04:10 -0300

    Added the logic to change between screen one and screen two

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import EnterScreen from "./EnterScreen"
+import React from "react";
+import EnterScreen from "./EnterScreen";
+import FlashScreen from "./FlashScreen";
 
 export default function App() {
-    return (
-        <div>
-            <EnterScreen />
-        </div>
-    )
+  const [page, setPage] = React.useState("screenOne");
+  console.log(page);
+  return page === "screenOne" ? (
+    <EnterScreen showScreenTwo={(screen) => setPage(screen)} />
+  ) : (
+    <FlashScreen />
+  );
 }
