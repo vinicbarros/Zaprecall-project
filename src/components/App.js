c08ab62287196fb34081da490032def7d883cfe7
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-07-22 11:11:57 -0300

    import css files

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import EnterScreen from "./EnterScreen";
 import FlashScreen from "./FlashScreen";
+import '../assets/css/reset.css';
+import '../assets/css/style.css';
 
 export default function App() {
   const [page, setPage] = React.useState("screenOne");
-  console.log(page);
   return page === "screenOne" ? (
     <EnterScreen showScreenTwo={(screen) => setPage(screen)} />
   ) : (
