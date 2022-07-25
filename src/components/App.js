bba670396a445ef4c8e1154c806251b75512372e
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-07-24 21:36:02 -0300

    Added the bonus to choose decks

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import EnterScreen from "./EnterScreen";
 import FlashScreen from "./FlashScreen";
-import '../assets/css/reset.css';
-import '../assets/css/style.css';
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
 
 export default function App() {
-  const [page, setPage] = React.useState("screenOne");
+  const [page, setPage] = useState("screenOne");
+  const [deckState, setDeckState] = useState("");
+
+  const deck1 = [
+    {
+      question: "O que é JSX?",
+      answer: "Uma extensão de linguagem do JavaScript",
+    },
+    {
+      question: "O React é __",
+      answer: "uma biblioteca JavaScript para construção de interfaces",
+    },
+    {
+      question: "Componentes devem iniciar com __ ",
+      answer: "letra maiúscula",
+    },
+    { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+    {
+      question: "O ReactDOM nos ajuda __",
+      answer: "interagindo com a DOM para colocar componentes React na mesma",
+    },
+    {
+      question: "Usamos o npm para __",
+      answer: "gerenciar os pacotes necessários e suas dependências",
+    },
+    {
+      question: "Usamos props para __ ",
+      answer: "passar diferentes informações para componentes",
+    },
+    {
+      question: "Usamos estado (state) para __",
+      answer:
+        "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+    },
+  ];
+
+  const deck2 = [
+    {
+      question: "O que é árvore DOM?",
+      answer:
+        "É a árvore de elementos do HTML representada como objetos no JavaScript",
+    },
+    {
+      question:
+        "Qual a melhor maneira de alterar estilos CSS através do JavaScript?",
+      answer: "Adicionando ou removendo classes conforme a necessidade",
+    },
+    {
+      question: "Qual o retorno da expressão banana * 3 ?",
+      answer: "NaN",
+    },
+    {
+      question: "O que é um evento no JavaScript?",
+      answer:
+        "Ações ou ocorrências que acontecem nos elementos acarretadas pelo usuário",
+    },
+    {
+      question: "O que são Promises no JavaScript?",
+      answer:
+        "Uma variavel que guarda uma promessa que depois de um tempo se tornará um valor ou um erro",
+    },
+    {
+      question: "Por que devemos usar promises/assincronismo?",
+      answer:
+        "Para não travar a execução do código quando temos códigos que demoram para se resolver",
+    },
+    {
+      question: "Qual diferença de requisições GET e POST?",
+      answer: "GET para pegar informações e POST para enviar informações",
+    },
+    {
+      question: "O que são Status Codes?",
+      answer:
+        "Códigos para dar mais informações sobre uma requisição (sucesso/erro de cliente/erro de servidor)",
+    },
+  ];
+  
   return page === "screenOne" ? (
-    <EnterScreen showScreenTwo={(screen) => setPage(screen)} />
+    <EnterScreen
+      showScreenTwo={(screen) => setPage(screen)}
+      deckState={deckState}
+      setDeckState={setDeckState}
+      deck1={deck1}
+      deck2={deck2}
+    />
   ) : (
-    <FlashScreen />
+    <FlashScreen
+      deckState={deckState}
+      setDeckState={setDeckState}
+      deck1={deck1}
+      deck2={deck2}
+    />
   );
 }
--- src/components/EnterScreen.js
+++ src/components/EnterScreen.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import bigLogo from '../assets/img/big-logo.png';
+import React from "react";
+import bigLogo from "../assets/img/big-logo.png";
 
-export default function EnterScreen({showScreenTwo}) {
+export default function EnterScreen({
+  showScreenTwo,
+  deckState,
+  setDeckState,
+  deck1,
+  deck2
+}) {
 
-    return (
-        <section className="screenOne">
-            <img src={bigLogo} alt=""/>
-            <h1>ZapRecall</h1>
-            <button onClick={()=> {
-                showScreenTwo("screenTwo");
-                }}><p>Iniciar Recall!</p></button>
-        </section>
-    )
+  return (
+    <section className="screenOne">
+      <img src={bigLogo} alt="" />
+      <h1>ZapRecall</h1>
+      <select
+        onChange={(e) => {
+          const selectedDeck = e.target.value;
+          if (selectedDeck === "deck1") {
+            setDeckState(deck1);
+          } else if (selectedDeck === "deck2") {
+            setDeckState(deck2);
+          }
+        }}
+      >
+        <option value="" selected disabled hidden>
+          Escolha um dos decks:
+        </option>
+        <option value="deck1">React</option>
+        <option value="deck2">Javascript</option>
+      </select>
+      <button
+        onClick={() => {
+          if (deckState === "") {
+            return alert("Vocẽ precisa escolher um dos decks!");
+          }
+          showScreenTwo("screenTwo");
+        }}
+      >
+        <p>Iniciar Recall!</p>
+      </button>
+    </section>
+  );
 }
